03a733febf6ab6c416842d490e89b99514e0277d
Alexandr Rebikov <rebikovlk@mail.ru>
2021-09-13 16:59:30 +0700

    step 4 - utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,15 +1,18 @@
 export function calcTileType(index, boardSize) {
-  console.log(boardSize);
-
   if (index === 0) {
     return 'top-left';
   } if (index > 0 && index < 7) {
     return 'top';
   } if (index === 7) {
     return 'top-right';
-  }
-  if (index === 56) {
+  } if (index > 7 && index < 56 && index % boardSize === 0) {
+    return 'left';
+  } if (index >= 15 && index <= 55 && (index + 1) % boardSize === 0) {
+    return 'right';
+  } if (index === 56) {
     return 'bottom-left';
+  } if (index > 56 && index < 63) {
+    return 'bottom';
   } if (index === 63) {
     return 'bottom-right';
   }
