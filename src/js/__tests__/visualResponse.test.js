2602337d19e99b4a7b75f8840ba991e816c41c8b
Alexandr Rebikov <rebikovlk@mail.ru>
2021-09-16 17:31:32 +0700

    add tests to point 10

---
+++ src/js/__tests__/visualResponse.test.js
@@ -0,0 +1,17 @@
+import GameController from '../GameController';
+
+jest.mock('../Gameplay');
+beforeEach(() => jest.resetAllMocks());
+
+test('should visual response cursors and cells', () => {
+  const gamePlay = new GamePlay();
+  const hero = new Bowerman(1);
+  const medal = '\u{1F396}';
+  const swords = '\u{2694}';
+  const shield = '\u{1F6E1}';
+  const heart = '\u{2764}';
+  const message = `${medal} ${hero.level} ${swords} ${hero.attack} ${shield} ${hero.defence} ${heart} ${hero.health}`;
+  gamePlay.showCellTooltip.mockReturnValue(message);
+
+  expect(gamePlay.showCellTooltip(message, 0)).toEqual('\u{1F396} 1 \u{2694} 25 \u{1F6E1} 25 \u{2764} 50');
+});
