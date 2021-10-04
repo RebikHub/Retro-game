a788f426f4fafc2aeff664db5961dbfb252ebb60
Alexandr Rebikov <rebikovlk@mail.ru>
2021-10-04 13:46:02 +0700

    fix error empty cell

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -316,7 +316,7 @@ export default class GameController {
         this.attackHero = GameController.cellsAttack(this.humanTeam[i], index);
       }
     }
-    if (['daemon', 'vampire', 'undead'].includes(cellClick.children[0].classList[1]) && !this.attackHero.includes(index)) {
+    if (['daemon', 'vampire', 'undead'].includes(cellClick?.children[0]?.classList[1]) && !this.attackHero.includes(index)) {
       GamePlay.showError('This hero cannot be selected!');
     }
   }
@@ -376,6 +376,8 @@ export default class GameController {
   }
 
   onNewGameClick() {
+    this.startPosHum = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
+    this.startPosAi = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
     this.startPosition = [];
     this.humanTeam = [];
     this.aiTeam = [];
